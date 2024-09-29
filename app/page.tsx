619771a66e1dea70628da40fe6d8f1199115fb33
Suman Jana <133626036+rocker1166@users.noreply.github.com>
2024-09-29 19:31:55 +0530

    Update page small text

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ const Home = () => {
     <main className="relative  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-<div class="flex items-center justify-center h-screen">
-  <p class="text-center text-sm text-white">Scroll Down ⬇️</p>
+<div className="flex items-center justify-center h-screen">
+  <p className="text-center text-sm text-white">Scroll Down ⬇️</p>
 </div>
 
         <AdvancedPortfolioHero />
